412452f3c2e72cdd9695eb7a9a2e1c36fc1451ad
Chris Greeff <chris.greeff@vendhq.com>
2013-10-26 13:24:16 +1300

    Updating markup for jacky to style up

--- web/js/cookbook.js
+++ web/js/cookbook.js
@@ -1,18 +1,20 @@
-var cookbookNode = $('#cookbook');
+var cookbookNode = $('.cookbook');
 
-cookbookNode.wysiwyg();
+if (cookbookNode.length) {
+    cookbookNode.wysiwyg();
 
-cookbookNode.on('keydown', function (event) {
-    if (event.keyCode === 13) {
-        if (event.shiftKey) {
-            event.preventDefault();
-            document.execCommand('insertUnorderedList');
-            document.execCommand('insertText', false, '\n\n');
-        } else if (event.ctrlKey || event.metaKey) {
-            event.preventDefault();
-            document.execCommand('insertUnorderedList');
-            document.execCommand('insertHTML', false, '<input type="checkbox"/>');
-            document.execCommand('insertText', false, '\n\n');
+    cookbookNode.on('keydown', function (event) {
+        if (event.keyCode === 13) {
+            if (event.shiftKey) {
+                event.preventDefault();
+                document.execCommand('insertUnorderedList');
+                document.execCommand('insertText', false, '\n\n');
+            } else if (event.ctrlKey || event.metaKey) {
+                event.preventDefault();
+                document.execCommand('insertUnorderedList');
+                document.execCommand('insertHTML', false, '<input type="checkbox"/>');
+                document.execCommand('insertText', false, '\n\n');
+            }
         }
-    }
-});
+    });
+}
