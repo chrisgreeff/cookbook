145486652fa2dea1fc7d7c94a88ee494880ce563
Chris Greeff <chris.greeff@vendhq.com>
2013-10-27 21:58:21 +1300

    Fixing up some styling

--- web/js/cookbook/cb-card-list-view.js
+++ web/js/cookbook/cb-card-list-view.js
@@ -1,17 +1,20 @@
 /*global YUI*/
 YUI.add('cb-card-list-view', function (Y) {
+    'use strict';
 
     var Micro = new Y.Template(),
 
         _renderCardList,
 
-        CLASS_NAMES = {};
+        CLASS_NAMES = {
+            card: 'cb-card'
+        };
 
     _renderCardList = Micro.compile(
         '<ul>' +
-            '<li class="{{CLASS_NAME.card}}">New Note</li>' +
+            '<li class="' + CLASS_NAMES.card + '">New Note</li>' +
             '<% Y.Array.each(this.cards, function(card) { %>' +
-                '<li class="{{CLASS_NAME.card}}">' +
+                '<li class="' + CLASS_NAMES.card + '">' +
                     '<%= card.content %>' +
                 '</li>' +
             '<% }); %>' +
@@ -28,7 +31,8 @@ YUI.add('cb-card-list-view', function (Y) {
 
         render: function () {
             this.get('container').setHTML(_renderCardList({
-                cards: this.get('modelList').toJSON()
+                cards: this.get('modelList').toJSON(),
+                CLASS_NAMES: CLASS_NAMES
             }));
 
             return this;
